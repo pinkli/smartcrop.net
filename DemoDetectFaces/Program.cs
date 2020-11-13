4470fce6f01d4f9876063651801ca6c0528a1eec
libin <pink.thereisnoif@gmail.com>
2020-11-13 17:09:26 +0800

    compute face rect in origin size

--- DemoDetectFaces/Program.cs
+++ DemoDetectFaces/Program.cs
@@ -25,16 +25,27 @@ namespace DemoDetectFaces
 
                 var faces = detector.Operator(img);
                 Console.WriteLine($"faces detected: {faces.Length}");
+
+                double size = img.Size;
+                double scale = Math.Sqrt(size0 / size);
+
                 foreach (var face in faces)
                 {
                     // draw it
                     Dlib.DrawRectangle(img, face, color: new RgbPixel(255, 0, 0), thickness: 4);
+                    Console.WriteLine($"left: {face.Left}, top: {face.Top}, width: {face.Width}, height: {face.Height}");
+
+                    int left = (int)(face.Left * scale);
+                    int top = (int)(face.Top * scale);
+                    int width = (int)(face.Width * scale);
+                    int height = (int)(face.Height * scale);
+                    var actualRect = new Rectangle(left, top, left + width, top + height);
+                    Console.WriteLine($"rect in origin img left: {actualRect.Left}, top: {actualRect.Top}, width: {actualRect.Width}, height: {actualRect.Height}");
                 }
 
                 if (faces.Length > 0)
                 {
-                    double size = img.Size;
-                    double scale = Math.Sqrt(size0 / size);
+                    
                     Dlib.ResizeImage(img, scale);
                     Dlib.SaveJpeg(img, $"{getFileName(imageFilePath)}-faces.jpg");
 
