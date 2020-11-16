f5f36b7296a5f8a3f5901b3cfa86d73b8e10476a
libin <pink.thereisnoif@gmail.com>
2020-11-16 18:33:41 +0800

    cropper cli tool

---
+++ Smartcrop/ImageCropAndSave.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Text;
+using System.IO;
+using SkiaSharp;
+
+
+namespace Smartcrop
+{
+    public class ImageCropAndSave
+    {
+        private string ratio;
+        private bool debug;
+
+        public ImageCropAndSave(string ratio, bool debug = false)
+        {
+            this.ratio = ratio;
+            this.debug = debug;
+        }
+
+        public void CropAndSave(string imagePath, string destFilePath, params BoostArea[] boostAreas)
+        {
+            var watch = Stopwatch.StartNew();
+
+            using (var bitmap = SKBitmap.Decode(imagePath))
+            {
+                var rationew = RatioUtil.ComputeRatio(bitmap.Width, bitmap.Height, this.ratio);
+                var options = new Options(rationew.Width, rationew.Height)
+                {
+                    Debug = this.debug
+                };
+
+                var crop = new ImageCrop(options);
+
+                // calculate the best crop area
+                var result = crop.Crop(bitmap, boostAreas);
+                watch.Stop();
+
+                Console.WriteLine($"cropCompute: {watch.ElapsedMilliseconds} ms");
+
+                watch.Restart();
+
+                // crop the image
+                SKRect cropRect = new SKRect(result.Area.Left, result.Area.Top, result.Area.Right, result.Area.Bottom);
+                using (SKBitmap croppedBitmap = new SKBitmap((int)cropRect.Width, (int)cropRect.Height))
+                using (SKCanvas canvas = new SKCanvas(croppedBitmap))
+                {
+                    SKRect source = new SKRect(cropRect.Left, cropRect.Top,
+                                            cropRect.Right, cropRect.Bottom);
+                    SKRect dest = new SKRect(0, 0, cropRect.Width, cropRect.Height);
+                    canvas.DrawBitmap(bitmap, source, dest);
+
+                    using (var image = SKImage.FromBitmap(croppedBitmap))
+                    using (var stream = File.Open(destFilePath, FileMode.OpenOrCreate))
+                    {
+                        image.Encode().SaveTo(stream);
+                        stream.Seek(0, SeekOrigin.Begin);
+
+                    }
+
+                    watch.Stop();
+
+                    Console.WriteLine($"drawCropped: {watch.ElapsedMilliseconds} ms");
+
+                };
+            }
+        }
+    }
+}
---
+++ smartcropper/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using Smartcrop;
+using McMaster.Extensions.CommandLineUtils;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Diagnostics;
+
+namespace smartcropper
+{
+    [Command(ExtendedHelpText = "face-aware smart image cropper")]
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CommandLineApplication.Execute<Program>(args);
+        }
+
+        [Option("--ratio <RATIO>", Description = "Dest image aspect ratio. Default 16:9")]
+        public string Ratio { get; set; }
+
+        [Option("--out-dir <DIR>", Description = "Output dir")]
+        public string OutDir { get; set; }
+
+        [Option("--no-detect-face", Description = "do not detect face")]
+        public bool NoDetectFace { get; set; }
+
+        [Argument(0, Description = "The image or folder to be cropped")]
+        [Required]
+        public string SourcePath { get; set; }
+
+        static string[] EXTS = new[] { ".JPG", ".JPEG", ".PNG", ".BMP"};
+
+        private DetectFaces facedetector = new DetectFaces();
+
+        private void OnExecute()
+        {
+            if (String.IsNullOrWhiteSpace(Ratio))
+            {
+                Ratio = "16:9";
+            }
+
+            if (String.IsNullOrWhiteSpace(OutDir))
+            {
+                OutDir = "./";
+            }
+
+            Console.WriteLine($"ratio: {Ratio}");
+            Console.WriteLine($"outdir: {OutDir}");
+            Console.WriteLine($"noface: {NoDetectFace}");
+            Console.WriteLine($"source path: {SourcePath}");
+
+            if (File.Exists(SourcePath))
+            {
+                var image = new FileInfo(SourcePath);
+                if (!IsValidImage(image))
+                {
+                    Console.WriteLine($"{SourcePath} is not applicable image");
+                    return;
+                }
+
+                CropImage(image);
+            }
+        }
+
+        private void CropImage(FileInfo imageFile)
+        {
+            // detect faces
+            var watch = Stopwatch.StartNew();
+            BoostArea[] boostAreas = Array.Empty<BoostArea>();
+            if (!this.NoDetectFace)
+            {
+                boostAreas = facedetector.FindBoostAreas(imageFile.FullName).ToArray();
+            }
+            watch.Stop();
+
+            Console.WriteLine($"{boostAreas.Length} faces detected, takes: {watch.ElapsedMilliseconds} ms");
+
+            // crop and save
+            var crop = new ImageCropAndSave(Ratio, debug: false);
+            string destFile = DestFile(imageFile);
+            crop.CropAndSave(imageFile.FullName, destFile, boostAreas);
+        }
+
+        private bool IsValidImage(FileInfo fi)
+        {
+            string ext = fi.Extension.ToUpperInvariant();
+            return EXTS.Contains(ext);
+        }
+
+        private string DestFile(FileInfo imageFile)
+        {
+            return Path.Combine(this.OutDir, imageFile.Name);
+        }
+    }
+}
