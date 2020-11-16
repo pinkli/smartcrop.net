7014824e20e11519a6bb8f6e4b9f4782f7f4bb4c
libin <pink.thereisnoif@gmail.com>
2020-11-16 19:36:56 +0800

    process directory

--- smartcropper/Program.cs
+++ smartcropper/Program.cs
@@ -22,8 +22,11 @@ namespace smartcropper
         [Option("--out-dir <DIR>", Description = "Output dir")]
         public string OutDir { get; set; }
 
-        [Option("--no-detect-face", Description = "do not detect face")]
+        [Option("--no-detect-face", Description = "Do not detect face")]
         public bool NoDetectFace { get; set; }
+
+        [Option("--skip-failed", Description = "Skip the crop-failing image, otherwise copy it to output dir")]
+        public bool SkipFailed { get; set; }
 
         [Argument(0, Description = "The image or folder to be cropped")]
         [Required]
@@ -35,12 +38,12 @@ namespace smartcropper
 
         private void OnExecute()
         {
-            if (String.IsNullOrWhiteSpace(Ratio))
+            if (string.IsNullOrWhiteSpace(Ratio))
             {
                 Ratio = "16:9";
             }
 
-            if (String.IsNullOrWhiteSpace(OutDir))
+            if (string.IsNullOrWhiteSpace(OutDir))
             {
                 OutDir = "./";
             }
@@ -49,6 +52,11 @@ namespace smartcropper
             Console.WriteLine($"outdir: {OutDir}");
             Console.WriteLine($"noface: {NoDetectFace}");
             Console.WriteLine($"source path: {SourcePath}");
+
+            if (!Directory.Exists(OutDir))
+            {
+                Directory.CreateDirectory(OutDir);
+            }
 
             if (File.Exists(SourcePath))
             {
@@ -61,25 +69,46 @@ namespace smartcropper
 
                 CropImage(image);
             }
+            else if (Directory.Exists(SourcePath))
+            {
+                var images = Directory.EnumerateFiles(SourcePath).Select(fi => new FileInfo(fi)).Where(fi => IsValidImage(fi));
+                foreach (var image in images)
+                {
+                    CropImage(image);
+                }
+            }
         }
 
         private void CropImage(FileInfo imageFile)
         {
-            // detect faces
-            var watch = Stopwatch.StartNew();
-            BoostArea[] boostAreas = Array.Empty<BoostArea>();
-            if (!this.NoDetectFace)
+            Console.WriteLine($"process image: {imageFile.FullName}");
+
+            try
             {
-                boostAreas = facedetector.FindBoostAreas(imageFile.FullName).ToArray();
+                // detect faces
+                var watch = Stopwatch.StartNew();
+                BoostArea[] boostAreas = Array.Empty<BoostArea>();
+                if (!this.NoDetectFace)
+                {
+                    boostAreas = facedetector.FindBoostAreas(imageFile.FullName).ToArray();
+                }
+                watch.Stop();
+
+                Console.WriteLine($"{boostAreas.Length} faces detected, takes: {watch.ElapsedMilliseconds} ms");
+
+                // crop and save
+                var crop = new ImageCropAndSave(Ratio, debug: false);
+                string destFile = DestFile(imageFile);
+                crop.CropAndSave(imageFile.FullName, destFile, boostAreas);
             }
-            watch.Stop();
-
-            Console.WriteLine($"{boostAreas.Length} faces detected, takes: {watch.ElapsedMilliseconds} ms");
-
-            // crop and save
-            var crop = new ImageCropAndSave(Ratio, debug: false);
-            string destFile = DestFile(imageFile);
-            crop.CropAndSave(imageFile.FullName, destFile, boostAreas);
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+                if (!SkipFailed)
+                {
+                    Copy(imageFile);
+                }
+            }
         }
 
         private bool IsValidImage(FileInfo fi)
@@ -91,6 +120,12 @@ namespace smartcropper
         private string DestFile(FileInfo imageFile)
         {
             return Path.Combine(this.OutDir, imageFile.Name);
+        }
+
+        private void Copy(FileInfo imageFile)
+        {
+            string dest = DestFile(imageFile);
+            File.Copy(imageFile.FullName, dest, true);
         }
     }
 }
